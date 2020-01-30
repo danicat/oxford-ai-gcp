27a2073a4acc953bbd3995130821f9340d214a69
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2020-01-30 12:28:10 +0000

    reorder demos, add bigquery

--- 01_wordcount/wordcount.py
+++ 01_wordcount/wordcount.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
-with open("hamlet.txt","r") as f:
-    wordcount={} 
-    for line in f.readlines():
-        # remove special characters
-        line = line.translate({ord(i):' ' for i in '!?@#$,.;:[]()'})
-        # normalise words to lowercase
-        line = line.lower()
 
-        for word in line.split():
-            if word not in wordcount:
-                wordcount[word] = 0
-            wordcount[word] += 1
+if __name__ == "__main__":
+    with open("hamlet.txt","r") as f:
+        wordcount={}
+        for line in f.readlines():
+            # remove special characters
+            line = line.translate({ord(i):' ' for i in '!?@#$,.;:[]()'})
+            # normalise words to lowercase
+            line = line.lower()
 
-    # sort by count descending and takes top 5 elements
-    top_words = sorted(wordcount.items(), key=lambda x: x[1], reverse=True)[:5]
+            for word in line.split():
+                if word not in wordcount:
+                    wordcount[word] = 0
+                wordcount[word] += 1
 
-    for word, count in top_words:
-        print(f"word: {word}, count: {count}")
+        # sort by count descending and takes top 5 elements
+        top_words = sorted(wordcount.items(), key=lambda x: x[1], reverse=True)[:5]
+
+        for word, count in top_words:
+            print(f"word: {word}, count: {count}")
+
